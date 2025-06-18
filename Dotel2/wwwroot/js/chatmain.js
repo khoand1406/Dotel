8f704dffd6a969f6c15cdd9c03b8fae0e3256826
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2025-06-19 01:09:22 +0700

    Create chatmain.js

---
+++ Dotel2/wwwroot/js/chatmain.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    this.querySelectorAll(".conversation-item-wrapper").forEach(item => {
+        item.addEventListener("click", function () {
+            const conversationId = this.getAttribute("data-convid");
+            fetch("?handler=LoadConversation", {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body:
+                    "conversationId=" + encodeURIComponent(conversationId)
+
+            }).then(response => response.text())
+                .then(html => {
+                    document.getElementById('chat-main').innerHTML = html;
+                    document.querySelectorAll(".conversation-item").forEach(el => el.classList.remove("active"));
+                    this.querySelector(".conversation-item").classList.add("active");
+                }).catch(error => {
+                    console.error('Failed to load conversation', error);
+                });
+        }
+        );
+    });
+});
